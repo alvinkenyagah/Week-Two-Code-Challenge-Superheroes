307adecda3620e432e4a05da1dd92e8796c72bcd
alvinkenyagah <alvin47k@gmail.com>
2023-07-03 13:24:11 +0300

    Update powers routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :heros, only: [:show, :index]
-  resources :powers
+  resources :powers,only: [:show, :index, :update]
   resources :hero_powers
 end
